ba7839db4d52f7a73c870e3666acab8305d243e9
vishal <vishal.kondle@gmail.com>
2024-06-23 00:05:14 +0530

    fix: todo component

--- app/(private)/todos/page.tsx
+++ app/(private)/todos/page.tsx
@@ -1,16 +1,22 @@
 'use client';
 
+import { Stack } from '@mantine/core';
 import { TodoPageActions } from '@/components/Todo';
-import Todos from '@/components/Todo/Todo';
 import TodoSkelton from '@/components/Todo/TodoSkelton';
 import useFetchData from '@/hooks/useFetchData';
+import { TodoType } from '@/models/Todo';
+import Todo from '@/components/Todo/Todo';
 
 const TodosPage = () => {
   const { data, refetch, loading } = useFetchData('/api/todos');
   return (
     <>
       <TodoPageActions refetch={refetch} />
-      {loading ? <TodoSkelton /> : <Todos todos={data} />}
+      {loading ? (
+        <TodoSkelton />
+      ) : (
+        <Stack>{data?.map((todo: TodoType) => <Todo todo={todo} />)}</Stack>
+      )}
     </>
   );
 };
--- components/Todo/Todo.tsx
+++ components/Todo/Todo.tsx
@@ -5,51 +5,47 @@ import dayjs from 'dayjs';
 import { TodoType } from '@/models/Todo';
 
 interface Props {
-  todos: TodoType[];
+  todo: TodoType;
 }
 
-const Todos = ({ todos }: Props) => (
-  <Stack>
-    {todos?.map((i: TodoType) => (
-      <Paper bg={`${i.color}.3`} p="md" key={String(i._id)}>
-        <Stack gap="xs">
-          <Group gap="xs" wrap="nowrap" justify="space-between">
-            <Group gap="xs" wrap="nowrap">
-              <ActionIcon color="gray.0" variant="transparent">
-                <IconCircleCheckFilled />
-              </ActionIcon>
-              <Text fw={700} c="gray.0" lineClamp={2}>
-                {i.todo}
-              </Text>
-            </Group>
-            <ActionIcon color="gray.0" variant="transparent">
-              <IconStarFilled />
-            </ActionIcon>
-          </Group>
-          <Group display={i?.list || i?.date ? 'flex' : 'none'}>
-            <Badge
-              c={i?.list?.color || 'dark'}
-              leftSection={<IconList size={14} />}
-              radius="xs"
-              variant="white"
-              display={i?.list ? 'block' : 'none'}
-            >
-              {i?.list?.title}
-            </Badge>
-            <Badge
-              c={i?.list?.color || 'dark'}
-              leftSection={<IconCalendar size={14} />}
-              radius="xs"
-              variant="white"
-              display={i?.date ? 'block' : 'none'}
-            >
-              {dayjs(i?.date).format('DD MMM YYYY')}
-            </Badge>
-          </Group>
-        </Stack>
-      </Paper>
-    ))}
-  </Stack>
+const Todo = ({ todo }: Props) => (
+  <Paper bg={`${todo?.color}.3`} p="md" key={String(todo?._id)}>
+    <Stack gap="xs">
+      <Group gap="xs" wrap="nowrap" justify="space-between">
+        <Group gap="xs" wrap="nowrap">
+          <ActionIcon color="gray.0" variant="transparent">
+            <IconCircleCheckFilled />
+          </ActionIcon>
+          <Text fw={700} c="gray.0" lineClamp={2}>
+            {todo?.todo}
+          </Text>
+        </Group>
+        <ActionIcon color="gray.0" variant="transparent">
+          <IconStarFilled />
+        </ActionIcon>
+      </Group>
+      <Group display={todo?.list || todo?.date ? 'flex' : 'none'}>
+        <Badge
+          c={todo?.list?.color || 'dark'}
+          leftSection={<IconList size={14} />}
+          radius="xs"
+          variant="white"
+          display={todo?.list ? 'block' : 'none'}
+        >
+          {todo?.list?.title}
+        </Badge>
+        <Badge
+          c={todo?.list?.color || 'dark'}
+          leftSection={<IconCalendar size={14} />}
+          radius="xs"
+          variant="white"
+          display={todo?.date ? 'block' : 'none'}
+        >
+          {dayjs(todo?.date).format('DD MMM YYYY')}
+        </Badge>
+      </Group>
+    </Stack>
+  </Paper>
 );
 
-export default Todos;
+export default Todo;
